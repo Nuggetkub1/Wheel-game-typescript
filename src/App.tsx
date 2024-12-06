9e837b698f2f496160ec77e6b780ae486a3fb625
Nuggetkub1 <nuggetzaza@gmail.com>
2024-12-06 21:13:04 +0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SpinWheel from "./components/SpinWheel";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="rainbow-title">Mod Wheel 556</h1>
+      <SpinWheel />
+    </div>
+  );
+}
+
+export default App;
